fdf2da3a0c802a167f510aaad9d63c35a2cb7ccb
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-10-21 18:15:01 +0300

    modified About.js

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,8 +4,11 @@ function About() {
   return (
   <>
     <img className="object-cover h-48 w-96" src="https://trello.com/1/cards/6352455bf4c39c009967a985/attachments/635288c74a99790283d6f2cc/previews/635288c84a99790283d6f2d8/download/aboutus.jpg" alt="about"/>
-    <img className="rounded-full object-cover h-48 w-96" src="https://trello.com/1/cards/6352455bf4c39c009967a985/attachments/635288e5c53ace0127a7f7d0/previews/635288e6c53ace0127a7f7d8/download/every.jpg" alt="about"/>
-    <div className="bg-[#e5e5e5] w-40">
+    <div className="text-center w-40">
+    <p >“You go through life wondering what is it all about life at the end of the day”</p>
+    </div>
+    <img className="rounded-full object-cover h-60 w-60" src="https://trello.com/1/cards/6352455bf4c39c009967a985/attachments/635288e5c53ace0127a7f7d0/previews/635288e6c53ace0127a7f7d8/download/every.jpg" alt="about"/>
+    <div className="bg-[#e5e5e5] w-60 text-center">
     <p >We provide a  solution to the pairing  problem. For 
         the teachers, leaders,lectures and other groups 
           who are involved in dealing   with users 
@@ -18,7 +21,7 @@ This gives efficiency in
 dealing with large number 
  of people.</p>
  </div>
-
+<div className="bg-[#0284c7] h-20"></div>
  
   </>
   )
