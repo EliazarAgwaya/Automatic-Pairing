10b5fba189a2f1c41c7cf13301560287061f9ddd
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-10-21 21:14:32 +0300

    modified Navbar.js

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,15 +5,20 @@ function About() {
     <>
   <div className="flex flex-row">
   <div className="ml-40">
-    <div className="mt-6">
-        <h1 className="">Automatic Pairing</h1>
-    <img className="object-cover h-48 w-96" src="https://trello.com/1/cards/6352455bf4c39c009967a985/attachments/635288c74a99790283d6f2cc/previews/635288c84a99790283d6f2d8/download/aboutus.jpg" alt="about"/>
+    <div className="mt-2">
+        <h1 className="text-[44px]">Automatic Pairing</h1>
+    <img className="object-cover h-40 w-96" src="https://trello.com/1/cards/6352455bf4c39c009967a985/attachments/635288c74a99790283d6f2cc/previews/635288c84a99790283d6f2d8/download/aboutus.jpg" alt="about"/>
     </div>
-    <div className="text-center w-40">
+    <div className="text-center w-60">
     <p >“You go through life wondering what is it all about life at the end of the day”</p>
     </div>
-    <img className="rounded-full object-cover h-28 w-28 mt-4" src="https://trello.com/1/cards/6352455bf4c39c009967a985/attachments/635288e5c53ace0127a7f7d0/previews/635288e6c53ace0127a7f7d8/download/every.jpg" alt="about"/>
+    <div className="">
+    Automation
+ </div>
+    <img className="rounded-full object-cover h-36 w-36" src="https://trello.com/1/cards/6352455bf4c39c009967a985/attachments/635288e5c53ace0127a7f7d0/previews/635288e6c53ace0127a7f7d8/download/every.jpg" alt="about"/>
     </div>
+    
+    
     <div className="bg-[#e5e5e5] w-80  text-center ml-80 mt-4 mb-4 ">
     <p >We provide a  solution to the pairing  problem. For 
         the teachers, leaders,<br></br>
@@ -30,6 +35,7 @@ This gives efficiency in
 dealing with large number <br></br>
  of people.</p>
  </div>
+ 
  </div>
 <div className="bg-[#0284c7] h-36"></div>
  
