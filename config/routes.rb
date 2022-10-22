8a8a8f3a7749c52a877244e5c413fe71b39eb18d
Denis <denismunene999@gmail.com>
2022-10-22 09:25:27 +0300

    add fallback action and route

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
   post '/login', to: 'student_sessions#create'
   delete '/logout', to: 'student_sessions#destroy'
 
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
