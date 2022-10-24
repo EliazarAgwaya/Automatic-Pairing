ea1c6cb8a658e0c54bf947518f3751518d019dff
Francis <francisodhiambo2@gmail.com>
2022-10-24 08:23:26 +0300

    Updated routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import React from "react";
-
-
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Login from "./components/Login"
+import Signup from "./components/Signup"
+import Footer  from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home />
       <Routes>
-        
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
+        <Route exact path="/Login" element={<Login />} />
+        <Route exact path="/Signup" element={<Signup />} />
       </Routes>
+      <Footer />
 
     </div>
   );
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
   <div className="home">
-    
+    <h1>Welcome to my home page</h1>
   </div>
   )
 }
