349c37a4f103d36d836fb06064b4ecabeb3e5c02
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-10-24 10:32:22 +0300

    Adds the structure of a student page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import React from "react";
 
-
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import ContactUs from "./components/ContactUs";
+import StudentPage from "./StudentPage";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <StudentPage />
+      <Navbar />
       <Routes>
-        
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
         <Route exact path="/contact" element={<ContactUs />} />
       </Routes>
-
     </div>
   );
 }
 
 export default App;
- 
---
+++ client/src/StudentPage.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function StudentPage() {
+  return (
+    <div>
+      <h1>AP</h1>
+      <h4>Welcome Sammy Kipkosgei</h4>
+      <img
+        src="https://img.freepik.com/free-photo/cheerful-young-caucasian-businessman_171337-727.jpg?size=626&ext=jpg&ga=GA1.2.1858690084.1658745301&semt=sph"
+        alt="Mentor"
+      />
+      <h3>Sammy Kipkosgei</h3>
+      <p>sammykipkosgei@gmail.com</p>
+      <p>Profile</p>
+      <p>Quizes</p>
+      <p>Logout</p>
+      <h2>You belong to the conquerers group</h2>
+      <h4>Tabitha Chepng'etich</h4>
+      <img
+        src="https://img.freepik.com/free-photo/cheerful-young-caucasian-businessman_171337-727.jpg?size=626&ext=jpg&ga=GA1.2.1858690084.1658745301&semt=sph"
+        alt="Pair"
+      />
+      <p>of email address tabithachepng'etich@gmail.com</p>
+      <p>Is your pair for this week</p>
+      <p>Your mentor is Dennis Munene </p>
+      <img
+        src="https://img.freepik.com/free-photo/cheerful-young-caucasian-businessman_171337-727.jpg?size=626&ext=jpg&ga=GA1.2.1858690084.1658745301&semt=sph"
+        alt="Pair"
+      />
+      <p>denniskariuki@gmail.com</p>
+      <h6>Feel free to reach out to us!</h6>
+      <h5>Visit Us Today</h5>
+      <p>Copyright: Automatic Pairing</p>
+      <h5>Get in touch</h5>
+      <p>Email Us</p>
+      <p>02-0000-1000</p>
+    </div>
+  );
+}
+export default StudentPage;
