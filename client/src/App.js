34e3b9ca9ef21da570af873e554a21ef07c7481f
Koima-Sam <sam17koima@gmail.com>
2022-10-28 22:11:09 +0300

    logout and autologin user

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 
 import Navbar from "./components/Navbar";
@@ -6,6 +6,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Footer from "./components/Footer";
 import ContactUs from "./components/ContactUs";
 import Students from "./components/Students";
 import StudentPage from "./StudentPage";
@@ -13,13 +14,32 @@ import Welcomepage from "./components/Welcomepage";
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
+  const[isMentor,setIsMentor] = useState(null);
   const [students, setStudents] = useState([]);
+  const navigate = useNavigate()
   useEffect(() => {
     // auto-login
     fetch("/me").then((r) => {
       if (r.ok) {
-        r.json().then((user) => setCurrentUser(user));
+        r.json().then((user) =>{ 
+          setCurrentUser(user)
+          setIsMentor(false)
+        });
       }
+
+    });
+  }, []);
+  // fetch logged in mentor
+  useEffect(() => {
+    // auto-login
+    fetch("/mentor").then((r) => {
+      if (r.ok) {
+        r.json().then((user) =>{ 
+          setCurrentUser(user)
+          setIsMentor(true)
+        });
+      }
+
     });
   }, []);
 
@@ -29,40 +49,50 @@ function App() {
       .then((students) => setStudents(students));
   }, []);
 
-  return (
-    <div className="App">
-      <Navbar />
-      <Welcomepage />
-      <StudentPage />
+
+  function handleLogout(){
+    fetch("/logout", { method: "DELETE" }).then((r) => {
+      if (r.ok) {
+        setCurrentUser(null);
+        setIsMentor(null)
+        navigate('/login')
+      }
+    });
+  }
+
+  if(!currentUser){
+    return(
+      <div className="App">
+
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<Home  currentUser={currentUser}/>} />
         {/* <Route exact path="/about" element={<About />} /> */}
         <Route
           exact
           path="/Login"
-          element={<Login setCurrentUser={setCurrentUser} />}
+          element={<Login setCurrentUser={setCurrentUser}  setIsMentor={setIsMentor}/>}
         />
         <Route
           exact
           path="/Signup"
-          element={<Signup setCurrentUser={setCurrentUser} />}
-        />
+          element={<Signup setCurrentUser={setCurrentUser} setIsMentor={setIsMentor} />}
+          />
         {/* <Route exact path="/contact" element={<ContactUs />} /> */}
 
-        <Route
-          exact
-          path="/students"
-          element={<Students students={students} setStudents={setStudents} />}
-        />
-        <Route
-          exact
-          path="/myprofile"
-          element={<StudentPage currentUser={currentUser} />}
-        />
       </Routes>
-      {/* <Footer /> */}
+    
     </div>
+    )
+
+  }else{
+
+  return (
+    <div className="app">
+     {isMentor? <Welcomepage currentUser={currentUser} setCurrentUser={setCurrentUser} handleLogout={handleLogout}/> :<StudentPage currentUser={currentUser} setCurrentUser={setCurrentUser}/>}
+    </div>
+  
   );
+  }
 }
 
 export default App;
