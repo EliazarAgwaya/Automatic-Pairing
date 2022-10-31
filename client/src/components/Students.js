b1a2c2e3d6a55bce012bf6490b409571ad2530c5
Koima-Sam <sam17koima@gmail.com>
2022-10-31 09:14:10 +0300

    add list of the students

--- client/src/components/Students.js
+++ client/src/components/Students.js
@@ -3,33 +3,37 @@ import React from "react";
 function Students({students}) {
   console.log((students))
   return (
-  <>
-    <h1 className="text-[44px] text-[#8F6107] ml-96 mt-20">List of all the Students</h1>
-    <div className="text-[20px] text-[#1D6697] ml-60 mt-20 flex flex-row">
+  <div>
+    <h1 className="text-[44px] text-[#8F6107]">List of all the Students</h1>
+    {/* <div className="text-[20px] text-[#1D6697] ">
       <h2 className="ml-6">Name</h2>
       <h2 className="ml-40">Email</h2>
       <h2 className="ml-36">Picture</h2>
       <h2 className="ml-60">Gender</h2>
-      </div>
-
-      <div className="text-[20px]  ml-60 mt-4 flex flex-row">
-        <ol >
-          {students.map((student)=>{
+      </div> */}
+      <table>
+        <tr>
+          <th>No</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Picture</th>
+          <th>Gender</th>
+        </tr>
+     
+          {students.map((a,index)=>{
             return(
-              <li>
-            <h5>{student.username}</h5>
-            <h5>{student.email}</h5>
-            <img className="ml-96" src={student.profile_image} alt="student" />
-            <p>
-              <strong>{student.gender}</strong>
-            </p>
-                </li>
+              <tr>
+
+          <td className='px-10'>{index+1}</td>
+          <td className='px-10'>{a.username}</td>
+          <td className='px-10'>{a.email}</td>
+          <td className='px-10'><img className='w-10 h-10 rounded-full' src={a.profile_image} /></td>
+          <td className='px-10'>{a.gender}</td>
+        </tr>
             )
           })}
-        </ol>
-
-      </div>
-  </>
+         </table>
+  </div>
   )
 }
 
